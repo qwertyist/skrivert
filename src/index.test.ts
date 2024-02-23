9707115ed05673bf9731e2c3bf4efa54dd8d8361
Botvid Johansson <asdf@qwertyist.se>
2024-02-23 17:10:19 +0100

    create basic tests for and draft caption parser

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,73 @@
 import { describe, it, expect } from 'vitest';
+import { parser } from "$lib/captions/main"
 
+describe("caption parser test 1", () => {
+    const options = {
+        chars: 37,  
+        lines: 2 
+    }
+    const text = "Mors lilla Olle i skogen gick, rosor på kind och solsken i blick"
+    const captions = {
+       blocks: [
+           { 
+               lines: [
+                   "Mors lilla Olle i skogen gick, rosor",
+                   "på kind och solsken i blick"
+                ]
+           }
+       ]
+    }
+    it("parses" + text + " to two lines", () => {
+        expect(parser(options, text)).toStrictEqual(captions)
+    })
+})
+
+describe("caption parser test 2", () => {
+    const options = {
+        chars: 32,  
+        lines: 2 
+    }
+    const text = "Mors lilla Olle i skogen gick, rosor på kind och solsken i blick"
+    const captions = {
+       blocks: [
+           { 
+               lines: [
+                   "Mors lilla Olle i skogen gick,",
+                   "rosor på kind och solsken i",
+                ]
+           },
+           { 
+               lines: [
+                   "blick",
+                ]
+           },
+       ]
+    }
+    it("parses" + text + " to 2 Blocks with 2 Lines", () => {
+        expect(parser(options, text)).toStrictEqual(captions)
+    })
+})
+describe("caption parser test 3", () => {
+    const options = {
+        chars: 32,  
+        lines: 3 
+    }
+    const text = "Mors lilla Olle i skogen gick, rosor på kind och solsken i blick"
+    const captions = {
+       blocks: [
+           { 
+               lines: [
+                   "Mors lilla Olle i skogen gick,",
+                   "rosor på kind och solsken i",
+                   "blick"
+                ]
+           },
+       ]
+    }
+    it("parses" + text + " to 1 Block with 3 Lines", () => {
+        expect(parser(options, text)).toStrictEqual(captions)
+    })
+})
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
---
+++ src/lib/captions/main.ts
@@ -0,0 +1,35 @@
+type parserOptions = {
+    lines: number;
+    chars: number;
+}
+
+type blockType = {
+    lines: string[];
+}
+
+type captionType = {
+    blocks: Array<blockType>;
+}
+const wrap = (s: string, w: number) => s.replace(
+    new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, 'g'), '$1\n'
+);
+
+export function parser(options: parserOptions, text: string): captionType {
+    const lines = wrap(text, options.chars).split("\n")
+    var buffer = lines
+    let blocks = [];
+
+    let block: blockType = {
+        lines: []
+    }
+
+    while (buffer.length > 0) {
+        block.lines = buffer.slice(0, options.lines) 
+        buffer = buffer.slice(options.lines)
+    }
+
+    const captions: captionType= {
+        blocks: [block]
+    }
+    return captions
+}
