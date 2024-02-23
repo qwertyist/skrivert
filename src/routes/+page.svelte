5c6d45bc493002aa3c51fe0307210f78b7835dd4
asdf qwertyist <asdf@qwertyist.se>
2024-02-23 14:43:34 +0100

    start with textarea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,19 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script lang="ts">
-  import { browser }from "$app/environment";
-  import { onMount, onDestroy } from "svelte";
-
-  export let options = { placeholder: "..." }
-  export let html ="<p>Vad står det här då?</p>"
-  let node;
-  let skrivert;
-
-  onMount(async () => {
-    const { default: Quill }= await import("quill");
-    const { default: Keyboard } = await import("../modules/keyboard.js");
-    Quill.register("modules/keyboard", Keyboard, true)
-    skrivert = new Quill(node, {
-      modules: {
-        toolbar: [],
-        keyboard: {
-          listIds: []
-        }
-      },
-      themes: "snow",
-      ...options
-    });
-  })
-  function handleClick() {
-    skrivert.focus();
-  };
+  let text;
 </script>
 
-<div class="container mx-auto h-full w-full" on:click={handleClick}>
-  <div class="editor" spellcheck=false bind:this={node} />
+
+<div class="container mx-auto h-full w-full">
+  <div contentEditable />
+
+  <div style="color: black;">
+    <textarea cols=80 rows=4 bind:value={text}/>
+  </div>
+
+  <div style="color: red;">
+    <textarea cols=80 rows=4 bind:value={text} />
+  </div>
 </div>
 
 <style lang="postcss">
