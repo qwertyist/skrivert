2acd76a3e98780eea89d6b6008e86f11ddc32296
Botvid Johansson <asdf@qwertyist.se>
2024-02-26 13:56:48 +0100

    adds function to merge shortform lists
    which returns the merged list with object containing conflicting
    shortforms with their respective phrases

---
+++ src/lib/shortforms/main.ts
@@ -0,0 +1,35 @@
+
+type ShortformExportType = {
+  sf: string;
+  p: string;
+  u: Date;
+};
+
+type ConflictType = {
+    sf: string;
+    ps: string[];
+
+ }
+
+export function merge(target: Array<ShortformExportType>, from: Array<ShortformExportType>) {
+    let merged: Array<ShortformExportType> = []
+    let sfs: string[] = []
+    let conflicts: Array<ConflictType> = []
+    target.forEach(item => {
+        sfs.push(item.sf)
+        merged.push(item)
+    })
+    from.forEach(item => {
+        if(sfs.indexOf(item.sf) != -1) {
+            const existsInTarget = target.filter(existing => { return existing.sf == item.sf})[0]
+            const conflict: ConflictType = {
+                sf: item.sf,
+                ps: [existsInTarget.p, item.p]
+            }
+            conflicts.push(conflict)
+        } else {
+            merged.push(item)
+        }
+    })
+    return { merged, conflicts }
+}
