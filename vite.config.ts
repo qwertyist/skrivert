52c889a229db2163d74bdcc30a3b7e1b2c048930
Botvid Johansson <asdf@qwertyist.se>
2024-02-26 13:00:51 +0100

    remove nightwatch

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,32 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+    test: {
+        coverage: {
+            exclude: 
+                [
+                 '**.config.**',
+                 'public/**',
+                 'TailwindCSS.svelte',
+                 /* ... default excludes */
+                 'coverage/**',
+                 'dist/**',
+                 '**/[.]**',
+                 'packages/*/test?(s)/**',
+                 '**/*.d.ts',
+                 '**/virtual:*',
+                 '**/__x00__*',
+                 '**/\x00*',
+                 'cypress/**',
+                 'test?(s)/**',
+                 'test?(-*).?(c|m)[jt]s?(x)',
+                 '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+                 '**/__tests__/**',
+                 '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+                 '**/vitest.{workspace,projects}.[jt]s?(on)',
+                 '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+                ]
+        }
+    }
 });
