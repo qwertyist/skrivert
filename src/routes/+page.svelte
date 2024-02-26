001e09854cbec6f8f6b1a4320ea479908a2b09d8
Botvid Johansson <asdf@qwertyist.se>
2024-02-26 20:50:31 +0100

    add some debug features to the main text view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,43 @@
 <script lang="ts">
   import { parser } from "$lib/captions/main"
   const parserOptions= {
-      lines: 2,
-      chars: 37
+    lines: 2,
+    chars: 37
   }
-  let text: string;
+  let buffer = [
+    { line: 1, content: "",}
+  ]
+  let text: string = "";
   let captions: string;
+  let beforeInputEvent: any = {}
+  function renderCaptions(content) {
+    captions = "";
+    const parsed = parser(parserOptions, content)
+    parsed.blocks.forEach(block => {
+      captions += block.lines.join("\n")
+      captions += "\n\n"
+    })
+  }
+  function updateBuffer() {
+    buffer[0] = text;
+    buffer.forEach(line => { 
+      renderCaptions(line);
+    })
+      
+  }
   function input(e) {
-      if(e.inputType == "insertLineBreak") {
-          captions = "";
-          const parsed = parser(parserOptions, text)
-          parsed.blocks.forEach(block => {
-              captions += block.lines.join("\n")
-              captions += "\n"
-          })
+    beforeInputEvent = e.inputType + " ("+e.data+")";
+    if(e.inputType == "insertText") {
+      console.log("Insert Text", e.data)
+      switch(e.data) {
+        case " ": {
+          updateBuffer()
+          break
+        }
       }
+    }
+    if(e.inputType == "insertLineBreak") {
+    }
   }
 </script>
 
@@ -26,6 +49,8 @@
       </div>
 
       <div> 
+        <span class="debug">beforeinput: {beforeInputEvent}</span><br />
+        <span class="debug">buffer: {buffer}</span><br />
         <textarea class="captions" cols=80 rows=4 bind:value={captions} />
       </div>
     </div>
