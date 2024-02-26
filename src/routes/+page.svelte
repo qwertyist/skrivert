56ff0fc9ce55ca3b302cf1084b41edd00b19a453
Botvid Johansson <asdf@qwertyist.se>
2024-02-26 14:27:44 +0100

    parse caption from textarea content on 'Enter'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,45 @@
 <script lang="ts">
-  let text;
+  import { parser } from "$lib/captions/main"
+  const parserOptions= {
+      lines: 2,
+      chars: 37
+  }
+  let text: string;
+  let captions: string;
+  function input(e) {
+      if(e.inputType == "insertLineBreak") {
+          captions = "";
+          const parsed = parser(parserOptions, text)
+          parsed.blocks.forEach(block => {
+              captions += block.lines.join("\n")
+              captions += "\n"
+          })
+      }
+  }
 </script>
 
 
 <div class="h-full w-full">
     <div class="h-full gap-4 grid grid-cols-2">
-      <div>
-        <textarea class="doc" bind:value={text}/>
+      <div class="docContainer" id="docContainer" on:beforeinput={input}>
+        <textarea bind:value={text}/>
       </div>
 
       <div> 
-        <textarea class="captions" cols=80 rows=4 bind:value={text} />
+        <textarea class="captions" cols=80 rows=4 bind:value={captions} />
       </div>
     </div>
 </div>
 
 <style lang="postcss">
-  .doc {
+  .docContainer {
     width: 100%;
     height: 100%;
-    color: black;
+  }
+  .docContainer textarea {
+    width: 100%;
+    height: 100%;
+    color: red;
     padding: 1em;
     resize: none;
   }
