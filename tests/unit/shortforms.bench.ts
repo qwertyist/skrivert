a3bbce7e9bf135fe4e838f594cfc1c26b9d87265
Botvid Johansson <asdf@qwertyist.se>
2024-04-19 10:00:20 +0200

    [test] bench merge two non-conflicting lists

--- tests/unit/shortforms.bench.ts
+++ tests/unit/shortforms.bench.ts
@@ -1,5 +1,8 @@
 import { bench, describe, it, expect } from "vitest";
+import { merge } from "$lib/shortforms/main";
 import PocketBase from "pocketbase";
+import default_shortforms from "../../src/data/shortforms.se.json";
+/*
 describe("[pb] get all shortforms from SE base list", async () => {
 const pb = new PocketBase("http://127.0.0.1:8090");
 try {
@@ -10,7 +13,7 @@ try {
   it("auths with admin account skrivertdevel@qwertyist.se", () => {
       expect(authData.admin.email).toBe("skrivertdevel@qwertyist.se");
   });
-} catch (err) {
+  } catch (err) {
   console.error("pb: Failed auth as admin:", err);
 }
   bench("retrieves the list of ~10000 shortforms", async () => {
@@ -19,3 +22,12 @@ try {
   });
   })
 });
+*/
+
+describe("merge lists", () => {
+const left = default_shortforms.slice(0,Math.ceil(default_shortforms.length/ 2))
+  const right = default_shortforms.slice(Math.ceil(default_shortforms.length / 2), default_shortforms.length );
+  bench("merge two lists with "+ left.length+ " + " + right.length + " shortforms with no conflicts", () => {
+    merge(left,right)
+  })
+})
